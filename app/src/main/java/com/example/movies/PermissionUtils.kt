be08124ef3904e64f053d59bab1eb16c7e64aef8
Carlos Buruel <carlos@rinro.com.mx>
2022-01-19 10:07:28 -0600

    request location permission

---
+++ app/src/main/java/com/example/movies/PermissionUtils.kt
@@ -0,0 +1,87 @@
+package com.example.movies
+
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.DialogFragment
+
+object PermissionUtils {
+	fun requestPermission(
+		activity: AppCompatActivity,
+		requestId: Int,
+		permission: String,
+		finishActivity: Boolean
+	) {
+		if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+			// Display a dialog with rationale.
+			RationaleDialog.newInstance(requestId, finishActivity)
+				.show(activity.supportFragmentManager, "dialog")
+		} else {
+			// Location permission has not been granted yet, request it.
+			ActivityCompat.requestPermissions(activity, arrayOf(permission), requestId)
+		}
+	}
+
+	fun isPermissionGranted(
+		grantPermission: Array<String>, grantResults: IntArray, permission: String): Boolean {
+		for (i in grantPermission.indices) {
+			if (permission == grantPermission[i]) {
+				return grantResults[i] == PackageManager.PERMISSION_GRANTED
+			}
+		}
+		return false
+	}
+
+
+	class RationaleDialog: DialogFragment() {
+		private var finishActivity = false
+		override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+			val requestCode = arguments?.getInt(ARGUMENT_PERMISSION_REQUEST_CODE) ?: 0
+			finishActivity = arguments?.getBoolean(ARGUMENT_FINISH_ACTIVITY) ?: false
+
+			return AlertDialog.Builder(activity)
+				.setMessage(R.string.permission_rationale_location)
+				.setPositiveButton(android.R.string.ok) { _, _ ->
+					ActivityCompat.requestPermissions(
+						requireActivity(),
+						arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+						requestCode
+					)
+					finishActivity = false
+				}
+				.setNegativeButton(android.R.string.cancel, null)
+				.create()
+		}
+
+		override fun onDismiss(dialog: DialogInterface) {
+			super.onDismiss(dialog)
+			if (finishActivity) {
+				Toast.makeText(activity, R.string.permission_required_toast, Toast.LENGTH_SHORT).show()
+			}
+			//activity?.finish()
+		}
+
+		companion object {
+			private const val ARGUMENT_PERMISSION_REQUEST_CODE = "requestCode"
+			private const val ARGUMENT_FINISH_ACTIVITY = "finish"
+
+			fun newInstance(requestCode: Int, finishActivity: Boolean): RationaleDialog {
+				val arguments = Bundle().apply {
+					putInt(ARGUMENT_PERMISSION_REQUEST_CODE, requestCode)
+					putBoolean(ARGUMENT_FINISH_ACTIVITY, finishActivity)
+				}
+				return RationaleDialog().apply {
+					this.arguments = arguments
+				}
+			}
+		}
+	}
+
+
+}
--- app/src/main/java/com/example/movies/ui/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/movies/ui/view/activity/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.movies.ui.view.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.movies.PermissionUtils.isPermissionGranted
+import com.example.movies.PermissionUtils.requestPermission
 import com.example.movies.R
 import com.example.movies.core.Constant.nameMovies
 import com.example.movies.core.Constant.nameMaps
@@ -14,56 +19,88 @@ import com.example.movies.databinding.ActivityMainBinding
 import com.example.movies.ui.view.fragment.MoviesFragment
 
 class MainActivity: AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private var lastFragment = ""
+	private lateinit var binding: ActivityMainBinding
+	private var lastFragment = ""
+	private var permissionDenied = false
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 
-        binding.run {
-            setSupportActionBar(toolbar)
-            lastFragment = getString(R.string.title_movies)
-            title = lastFragment
-            addFragment(MoviesFragment())
-        }
-    }
+		binding.run {
+			setSupportActionBar(toolbar)
+			lastFragment = getString(R.string.title_movies)
+			title = lastFragment
+			addFragment(MoviesFragment())
+		}
+		enableLocation()
+	}
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		menuInflater.inflate(R.menu.main, menu)
+		return true
+	}
 
-    override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
-        R.id.iMovies -> {
-            if (lastFragment != nameMovies) {
-                lastFragment = nameMovies
-                addFragment(MoviesFragment())
-            }
-            true
-        }
-        R.id.iMaps -> {
-            if (lastFragment != nameMaps) {
-                lastFragment = nameMaps
-                addFragment(MoviesFragment())
-            }
-            true
-        }
-        R.id.iImages -> {
-            if (lastFragment != nameImages) {
-                lastFragment = nameImages
-                addFragment(MoviesFragment())
-            }
-            true
-        }
-        else -> super.onOptionsItemSelected(item)
-    }
+	override fun onOptionsItemSelected(item: MenuItem) = when(item.itemId) {
+		R.id.iMovies -> {
+			if (lastFragment != nameMovies) {
+				lastFragment = nameMovies
+				addFragment(MoviesFragment())
+			}
+			true
+		}
+		R.id.iMaps -> {
+			if (lastFragment != nameMaps) {
+				lastFragment = nameMaps
+				addFragment(MoviesFragment())
+			}
+			true
+		}
+		R.id.iImages -> {
+			if (lastFragment != nameImages) {
+				lastFragment = nameImages
+				addFragment(MoviesFragment())
+			}
+			true
+		}
+		else -> super.onOptionsItemSelected(item)
+	}
 
-    private fun addFragment(fragment: Fragment) {
-        addFragment(
-            supportFragmentManager,
-            fragment,
-            binding.fragmentContainer.id)
-    }
+	private fun addFragment(fragment: Fragment) {
+		addFragment(
+			supportFragmentManager,
+			fragment,
+			binding.fragmentContainer.id)
+	}
+
+	override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<String>,
+		grantResults: IntArray
+	) {
+		if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+			super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+			return
+		}
+		if (isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+			enableLocation()
+		} else {
+			permissionDenied = true
+		}
+	}
+
+	private fun enableLocation() {
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+			== PackageManager.PERMISSION_GRANTED) {
+			//TODO read location
+		} else {
+			requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+				Manifest.permission.ACCESS_FINE_LOCATION, true)
+		}
+	}
+
+	companion object {
+		private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+	}
 }
