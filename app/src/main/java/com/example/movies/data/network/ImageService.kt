77730b7a00bcc2363b0f656a6aac4fcdd68ad4e8
Carlos Buruel <carlos@rinro.com.mx>
2022-01-23 06:52:31 -0600

    load images & upload images

--- app/src/main/java/com/example/movies/data/network/ImageService.kt
+++ app/src/main/java/com/example/movies/data/network/ImageService.kt
@@ -1,13 +1,20 @@
 package com.example.movies.data.network
 
+import android.graphics.Bitmap
+import android.util.Log
 import com.example.movies.ui.view.fragment.images.ImageContract
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ImageService {
 	private val database = Firebase.firestore
 
-	fun getLocation(view: ImageContract) {
+	fun getImages(view: ImageContract) {
 		database.collection("images")
 			.get()
 			.addOnSuccessListener { result ->
@@ -20,6 +27,48 @@ class ImageService {
 			}
 			.addOnFailureListener { exception ->
 				exception.toString()
+			}
+	}
+
+	fun uploadBitmap(bitmap: Bitmap) {
+		val output = ByteArrayOutputStream()
+		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, output)
+		val data1 = output.toByteArray()
+
+		val storage = Firebase.storage("gs://movies-maps-and-photos.appspot.com")
+		val storageRef = storage.reference
+
+		val imageRef = storageRef.child("images/${UUID.randomUUID()}.jpg")
+
+		val uploadTask = imageRef.putBytes(data1)
+		uploadTask.addOnFailureListener { error ->
+			error.toString()
+		}
+		uploadTask.addOnSuccessListener { taskSnapshot ->
+			taskSnapshot.toString()
+			registerUrl(imageRef)
+		}
+	}
+
+	private fun registerUrl(reference: StorageReference) {
+		reference.downloadUrl.addOnSuccessListener {
+			registerUrl("$it")
+		}.addOnFailureListener {
+			it.toString()
+		}
+	}
+
+	private fun registerUrl(url: String) {
+		val imageData = hashMapOf(
+			"url_image" to url
+		)
+		database.collection("images")
+			.add(imageData)
+			.addOnSuccessListener { documentReference ->
+				Log.d("IMAGE_ID", documentReference.id)
+			}
+			.addOnFailureListener { error ->
+				Log.d("IMAGE_ID", "Error registered document", error)
 			}
 	}
 }
--- app/src/main/java/com/example/movies/ui/view/fragment/images/ImageFragment.kt
+++ app/src/main/java/com/example/movies/ui/view/fragment/images/ImageFragment.kt
@@ -1,9 +1,17 @@
 package com.example.movies.ui.view.fragment.images
 
+import android.content.ContentResolver
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.movies.data.network.ImageService
@@ -11,6 +19,7 @@ import com.example.movies.databinding.FragmentImageGaleryBinding
 
 class ImageFragment: Fragment(), ImageContract {
 	private lateinit var binding: FragmentImageGaleryBinding
+	private val imageService = ImageService()
 	private lateinit var imagesAdapter: ImagesAdapter
 	private val aUrlImages = ArrayList<String>()
 
@@ -23,11 +32,36 @@ class ImageFragment: Fragment(), ImageContract {
 		return binding.root
 	}
 
+	private val getResult = registerForActivityResult(
+		ActivityResultContracts.StartActivityForResult()
+	) { activityResult ->
+		val data = activityResult.data
+		data?.data?.let { contentURI ->
+			val bitmap = getBitmap(requireActivity().contentResolver, contentURI)
+			imageService.uploadBitmap(bitmap)
+		} ?: run {
+			data?.clipData?.let { clipData ->
+				for (index in 0 until clipData.itemCount) {
+					val item = clipData.getItemAt(index)
+					val uri = item.uri
+					val bitmap = getBitmap(requireActivity().contentResolver, uri)
+					imageService.uploadBitmap(bitmap)
+				}
+			}
+		}
+	}
+
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		ImageService().getLocation(this)
+		ImageService().getImages(this)
 		binding.run {
 			tvUpload.setOnClickListener {
-
+				val intentImage = Intent().apply {
+					type = "image/*"
+					putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+					action = Intent.ACTION_GET_CONTENT
+				}
+				val intent = Intent.createChooser(intentImage, "Elija sus photos")
+				getResult.launch(intent)
 			}
 		}
 	}
@@ -40,6 +74,16 @@ class ImageFragment: Fragment(), ImageContract {
 		binding.run {
 			rvImages.layoutManager = GridLayoutManager(requireActivity(), 2, GridLayoutManager.VERTICAL, false)
 			rvImages.adapter = imagesAdapter
+		}
+	}
+
+	@Suppress("deprecation")
+	private fun getBitmap(contentResolver: ContentResolver, uri: Uri): Bitmap {
+		return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+			val source = ImageDecoder.createSource(contentResolver, uri)
+			ImageDecoder.decodeBitmap(source)
+		} else {
+			MediaStore.Images.Media.getBitmap(contentResolver, uri)
 		}
 	}
 }
